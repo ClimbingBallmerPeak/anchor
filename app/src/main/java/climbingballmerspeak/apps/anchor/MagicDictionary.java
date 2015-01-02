1c2794d049e34f21c6b40c715af8f28e3c708812
Thomas <thutch77@gmail.com>
2015-01-01 23:20:46 -0500

    Untested ListView Page Code

--- app/src/main/java/climbingballmerspeak/apps/anchor/MagicDictionary.java
+++ app/src/main/java/climbingballmerspeak/apps/anchor/MagicDictionary.java
@@ -1,10 +1,18 @@
 package climbingballmerspeak.apps.anchor;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MagicDictionary extends ActionBarActivity {
@@ -38,7 +46,67 @@ public class MagicDictionary extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /** Called when the user clicks the Begin button */
+    /**
+     * Called when the user clicks the Begin button
+     */
+    public void loadMacroListView(View view) {
+        //  switch to  listview screen
+        setContentView(R.layout.macro_list_view);
+        //  localize listview for referencing
+        final ListView listview = (ListView) findViewById(R.id.macroListView);
+        //  Set up a dummy array for display testing
+        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile"};
+        //  Create an Arraylist for use in the listview
+        final ArrayList<String> list = new ArrayList<String>();
+        // Populate the new ArrayList from the string array
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+    }
+
+    /**
+     * Called when the user chooses a listview item
+     */
     public void loadMagicDictionary(View view) {
         setContentView(R.layout.activity_magic_dictionary);
     }
